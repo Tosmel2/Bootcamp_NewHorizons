8c401e39e77e4f20feeb4249d030d63edfe38cb2
tosmel2 <adewaletosin0808@gmail.com>
2022-01-12 23:32:59 +0100

    bootcamp webpage

---
+++ mail.php
@@ -0,0 +1,22 @@
+<?php 
+
+    var_dump($_POST)
+    if(isset($_POST['submit'])){ 
+        echo "Hello"
+        $to = "tosmelt@gmail.com"; // this is the to address 
+        $from = $_POST['email']; // this is the sender's Email address 
+        $name = $_POST['name']; 
+		$message = $_POST['message']; 
+        $subject = "Message"; 
+        $body = "Name: ". $name. "\n From: " . $_POST['email'] . "\n Message: " . $_POST['message']; 
+
+        $headers = "From:" . $from; 
+        $mail=mail($to,$subject,$body,$headers); 
+		if($mail){ 
+			echo "Mail Sent. Thank you " ; 
+		}else{ 
+			echo "Mail sending failed"; 
+		} 
+    } 
+
+?>
