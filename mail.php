1fe5858140a1ac69430bf94caddef39ec1da6013
adedejiibrahim7 <adedejiibrahim7@gmail.com>
2022-01-13 00:49:20 +0100

    updates

--- mail.php
+++ mail.php
@@ -1,16 +1,15 @@
 <?php 
-
-    var_dump($_POST)
+    ini_set( 'display_errors', 1 );
     if(isset($_POST['submit'])){ 
-        echo "Hello"
+        // echo "Hello";
         $to = "tosmelt@gmail.com"; // this is the to address 
         $from = $_POST['email']; // this is the sender's Email address 
         $name = $_POST['name']; 
-		$message = $_POST['message']; 
-        $subject = "Message"; 
-        $body = "Name: ". $name. "\n From: " . $_POST['email'] . "\n Message: " . $_POST['message']; 
-
-        $headers = "From:" . $from; 
+		// $message = $_POST['message']; 
+        $subject = "Register"; 
+        $body = "Name: ". $name. "\n From: " . $_POST['email'] . "\n Phone: " . $_POST['phone']; 
+        $headers = '';
+        $headers .= "From:" . $from; 
         $mail=mail($to,$subject,$body,$headers); 
 		if($mail){ 
 			echo "Mail Sent. Thank you " ; 
