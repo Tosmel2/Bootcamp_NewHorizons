58d29bde0380b36734f68dfe35e53b4cf3de9e1e
tosmel2 <adewaletosin0808@gmail.com>
2022-09-05 17:02:33 +0100

    card title localStorage fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 AOS.init({
     duration: 1200,
 })
+
+
+const courses = document.querySelectorAll('.course');
+
+courses.forEach(course => {
+    course.addEventListener('click', (e) => {
+        const courseName = e.currentTarget.previousElementSibling.textContent;
+        localStorage.setItem('card-title', courseName);
+    })
+})
